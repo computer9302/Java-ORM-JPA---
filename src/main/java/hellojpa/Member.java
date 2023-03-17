e2b11201d85047e61e04fa7400bc5795680ad1b0
computer9302 <computer9302@gmail.com>
2023-03-17 16:41:52 +0900

    자바 ORM 표준 JPA 프로그래밍 - 엔티티 매핑(기본키 매핑) 자바 ORM 표준 JPA 프로그래밍 - 기본편(일대다)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -21,12 +21,6 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    //@Column(name="TEAM_ID")
-    //private Long teamId;
-
-    @ManyToOne
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
 
     public long getId() {
         return id;
@@ -44,11 +38,5 @@ public class Member {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
-    }
 
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 }
