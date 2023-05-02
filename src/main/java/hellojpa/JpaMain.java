810b51a67515319764191052f032d00f9f49af0b
computer9302 <computer9302@gmail.com>
2023-05-02 21:45:33 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편(객체지향 쿼리 언어, JPQL, Criteria)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,9 @@
 package hellojpa;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -17,35 +20,20 @@ public class JpaMain {
 
         try{
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            //Creteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            member.getFaveritFoods().add("치킨");
-            member.getFaveritFoods().add("족발");
-            member.getFaveritFoods().add("피자");
+            Root<Member> m = query.from(Member.class);
 
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+            CriteriaQuery<Member> cq = query.select(m);
 
-            em.persist(member);
+            String username = "dadaa";
+            if(username != null){
+                cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            }
 
-            em.flush();
-            em.clear();
-
-            System.out.println("======================START=====================");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-//            findMember.getFaveritFoods().remove("치킨");
-//            findMember.getFaveritFoods().add("한식");
-
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
         }catch(Exception e){
